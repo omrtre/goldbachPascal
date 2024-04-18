3de715cc8471d106c2a0cb8e4641ce65f332b712
omrtre <mr.omr.tre@gmail.com>
2024-04-18 12:06:02 -0400

    StarterCode

--- goldbach.pp
+++ goldbach.pp
@@ -1,5 +1,50 @@
 program Goldbach;
 
+// import sys
+uses 
+    SysUtils; 
+
+// Create some functions to handle "gettingPrimes", "goldBach", "read n write", "printing", and "mainLoop"
+
+// GetPrime
+// Is this a Prime number? Boolean is True // False
+function getPrime(num: Integer): Boolean;
+var n: Integer;
+begin
+
+end;
+
+
+// Goldbach
+// Find the pairs, set them up
+procedure goldBach(n: Integer; var pairCount: Integer);
+var
+    a, b, c: Integer;
+begin
+
+end;
+
+
+function printGoldBach()
+
+
+
+// Compute for one or more input values
+
+// For each input value, utput a statement reporting how many Goldbach pairs have been found, before outputting the found Goldbach pairs
+// store all found Goldbach pairs so you can count them before outputting
+
+// output one line for each pair of the found Goldbach pairs
+// form of <input value> = <p> + <q>, with p <= q. The lines should be arranged in increasing order of <p>
+
+// when giving no command line argument, program should output the Goldbach pairs for the input values of 3, 4, 14, 26, and 100
+
+// given one or more command line arguments, your program should assume that the first argument to be the name of an input file, and process all numbers stored in that file
+
+// assume that the input file always exists, and it contains data in the correct format
+
+// assume that the input file contains one number per line, and those numbers ae always even integers between 4 to 100,000 (inclusive)
+// umber of lines in the file is NOT known in advance, and the end of the file signals the end of input.
 begin
     writeln('HelloWorld');
 end.
